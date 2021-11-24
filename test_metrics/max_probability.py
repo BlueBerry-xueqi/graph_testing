b9bb2c7559fb54d1b76e1d2432e618fe6097563a
snow77777 <56833189+BlueBerry-xueqi@users.noreply.github.com>
2021-11-24 18:19:15 +0100

    max metrics change

--- test_metrics/max_probability.py
+++ test_metrics/max_probability.py
@@ -1,8 +1,21 @@
+import pdb
 import numpy as np
 import torch
 
-def max_score(model, target_data):
-    pre_prob, pre_labels, ground_truth = model.predict(target_data)
-    pre_prob = np.exp(pre_prob)
-    maxP, _ = torch.max( pre_prob, 1)
-    return maxP, pre_labels, ground_truth 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def max_score(model, target_loader, select_num):
+    scores = []
+    with torch.no_grad():
+        for data in target_loader:
+            data = data.to(device)
+            output = model(data.x, data.edge_index, data.batch)
+            y_pred = torch.softmax(output, dim=1)
+            score = np.max(torch.Tensor.cpu(y_pred).numpy(), axis=1)
+            scores = np.concatenate((scores, score))
+
+    scores_sort = np.argsort(scores)[::-1]
+    select_index = scores_sort[-select_num:]
+    select_index = torch.from_numpy(np.flip(select_index, axis=0).copy())
+    return select_index
