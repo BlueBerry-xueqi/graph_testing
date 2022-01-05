48275f426455367d441c63021495fa6069458483
BlueBerry-xueqi <56833189+BlueBerry-xueqi@users.noreply.github.com>
2022-01-05 10:43:38 +0100

    RQ2 files

---
+++ RQ2/selection_mnist.py
@@ -0,0 +1,126 @@
+import copy
+import math
+import os
+import sys
+
+import numpy as np
+import torch
+from torch_geometric.loader import DataLoader
+
+from models.geometric_models.mnist_nn_conv import train as MNIST_train, test as MNIST_test
+from parser import Parser
+from trainer_mnist import construct_model
+
+from test_metrics.MNIST_metrics.M_Margin import margin_metrics
+from test_metrics.MNIST_metrics.M_random import random_select
+from test_metrics.MNIST_metrics.M_Entropy import entropy_metrics
+from test_metrics.MNIST_metrics.M_DeepGini import DeepGini_metrics
+from test_metrics.MNIST_metrics.M_least_confidence import least_confidence_metrics
+
+from test_metrics.MNIST_metrics.GMM import GMM_metrics
+from test_metrics.MNIST_metrics.Agg import AgglomerativeClustering_metrics
+from test_metrics.MNIST_metrics.Kmeans_selection import KMeans_score
+from test_metrics.MNIST_metrics.MCP import mcp_score
+from test_metrics.MNIST_metrics.Spectral import SpectralClustering_metrics
+from test_metrics.MNIST_metrics.variance import computeVariancescore
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def loadData():
+    savedpath_index = f"pretrained_all/pretrained_model/{args.type}_{args.data}/index"
+
+    if not (os.path.isfile(f"{savedpath_index}/train_index.pt") and os.path.isfile(
+            f"{savedpath_index}/val_index.pt") and os.path.isfile(
+        f"{savedpath_index}/test_index.pt")):
+        print("No prepared index")
+    else:
+        dataset = torch.load(f"{savedpath_index}/dataset.pt")
+        train_index = torch.load(f"{savedpath_index}/train_index.pt")
+        val_index = torch.load(f"{savedpath_index}/val_index.pt")
+        test_index = torch.load(f"{savedpath_index}/test_index.pt")
+        # get dataset
+        train_dataset = dataset[train_index]
+        val_dataset = dataset[val_index]
+        test_dataset = dataset[test_index]
+
+        train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    return dataset, train_index, test_index, train_loader, val_loader, test_loader, train_dataset, val_dataset, test_dataset
+
+
+def retrain_and_save(args):
+    dataset, train_index, test_index, train_loader, val_loader, test_loader, train_dataset, val_dataset, test_dataset = loadData()
+    mse_list = []
+
+    # do three exps
+    for exp_ID in range(100):
+        model, optimizer = construct_model(args, dataset)
+        model = model.to(device)
+
+        savedpath_model = f"pretrained_all/pretrained_model/{args.type}_{args.data}/"
+        model.load_state_dict(torch.load(os.path.join(savedpath_model, "model.pt"), map_location=device))
+
+        ratio = args.select_ratio
+        len_test = len(test_index)
+        select_num = int(ratio / 100 * len_test)
+
+        # get select index using metrics
+        select_index = select_functions(model, len_test, args.metrics, test_loader, select_num, dataset)
+        sample_index = test_index[select_index]
+        sample_dataset = dataset[sample_index]
+        sample_loader = DataLoader(sample_dataset, batch_size=64, shuffle=True)
+        sample_test_acc = MNIST_test(sample_loader, sample_dataset, model)
+
+        savedpath_acc = f"pretrained_all/train_accuracy/{args.type}_{args.data}/test_accuracy.npy"
+        test_acc = np.load(savedpath_acc)
+        diff = test_acc - sample_test_acc
+        mse_power = np.power(diff, 2)
+        mse_list.append(mse_power)
+
+        print(f'Exp: {exp_ID:03d},  Test acc: {test_acc:.4f}, sample acc: {sample_test_acc:.4f}, '
+              f'difference: {diff:.4f}')
+
+    mes_avg = math.sqrt(np.sum(mse_list) / 100)
+    print("mse is: ", mes_avg)
+
+    # save best accuracy
+    savedpath_avg = f"MSE/{args.type}_{args.data}/{args.metrics}/"
+    if not os.path.isdir(savedpath_avg):
+        os.makedirs(savedpath_avg, exist_ok=True)
+    np.save(f"{savedpath_avg}/mse{ratio}.npy", mes_avg)
+
+
+def select_functions(model, retrain_dataset_length, metric, retrain_loader, select_num, dataset):
+    if metric == "deepgini":
+        selected_index = DeepGini_metrics(model, retrain_loader, select_num)
+    elif metric == "random":
+        selected_index = random_select(retrain_dataset_length, select_num)
+    elif metric == "l_con":
+        selected_index = least_confidence_metrics(model, select_num, retrain_loader)
+    elif metric == "entropy":
+        selected_index = entropy_metrics(model, retrain_loader, select_num)
+    elif metric == "margin":
+        selected_index = margin_metrics(model, retrain_loader, select_num)
+
+    elif metric == "MCP":
+        selected_index = mcp_score(model, retrain_loader, select_num, dataset.num_classes, 64)
+    elif metric == "variance":
+        selected_index = computeVariancescore(model, retrain_loader, select_num)
+    elif metric == "kmeans":
+        selected_index = KMeans_score(model, retrain_loader, select_num)
+    elif metric == "spec":
+        selected_index = SpectralClustering_metrics(model, retrain_loader, select_num)
+    elif metric == "Hierarchical":
+        selected_index = AgglomerativeClustering_metrics(model, retrain_loader, select_num)
+    elif metric == "GMM":
+        selected_index = GMM_metrics(model, retrain_loader, select_num)
+
+    return selected_index
+
+
+if __name__ == "__main__":
+    args = Parser().parse()
+    retrain_and_save(args)
---
+++ RQ2/trainer_mnist_gra.py
@@ -0,0 +1,104 @@
+import copy
+import os
+import os.path as osp
+
+import numpy as np
+import torch
+import torch_geometric.transforms as T
+from torch_geometric.datasets import MNISTSuperpixels
+from torch_geometric.loader import DataLoader
+
+from models.geometric_models.mnist_graclus import Net as graclus, train as graclus_train, test as graclus_test
+from parser import Parser
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# model: NN, dataset: MNIST
+
+# construct the original model
+def construct_model(args, dataset):
+    if args.type == "gra":
+        model = graclus(dataset)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    return model, optimizer
+
+
+def load_data(args):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'MNIST')
+    transform = T.Cartesian(cat=False)
+    dataset = MNISTSuperpixels(path, True, transform=transform)
+    dataset = dataset.shuffle()
+    dataset = dataset[:5000]
+    print("load data MNIST successfully!")
+
+    n = len(dataset) // 10
+    index = torch.randperm(len(dataset))
+    train_index, val_index, test_index = index[:6 * n], index[6 * n:8 * n], index[8 * n:]
+    # get dataset
+    train_dataset = dataset[train_index]
+    val_dataset = dataset[val_index]
+    test_dataset = dataset[test_index]
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    # save index path
+    savedpath_index = f"pretrained_all/pretrained_model/{args.type}_{args.data}/index"
+    if not os.path.isdir(savedpath_index):
+        os.makedirs(savedpath_index, exist_ok=True)
+
+    # save index
+    torch.save(dataset, f"{savedpath_index}/dataset.pt")
+    torch.save(train_index, f"{savedpath_index}/train_index.pt")
+    torch.save(val_index, f"{savedpath_index}/val_index.pt")
+    torch.save(test_index, f"{savedpath_index}/test_index.pt")
+
+    return dataset, train_index, test_index, train_loader, val_loader, test_loader, train_dataset, val_dataset, test_dataset
+
+
+def train_and_save(args):
+    dataset, train_index, test_index, train_loader, val_loader, test_loader, train_dataset, val_dataset, test_dataset = load_data(
+        args)
+
+    # construct model
+    model, optimizer = construct_model(args, dataset)
+    model = model.to(device)
+
+    best_acc = 0
+
+    for epoch in range(args.epochs):
+        graclus_train(epoch, model, optimizer, train_loader)
+        train_acc = graclus_test(model, train_loader, train_dataset)
+        val_acc = graclus_test(model, val_loader, val_dataset)
+        # select best test accuracy and save best model
+        if val_acc > best_acc:
+            best_acc = val_acc
+            best_model = copy.deepcopy(model.state_dict())
+
+        print(f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
+              f'Val: {val_acc:.4f}')
+    print("Best val accuracy is: ", best_acc)
+
+    # save best model
+    savedpath_model = f"pretrained_all/pretrained_model/{args.type}_{args.data}/"
+    if not os.path.isdir(savedpath_model):
+        os.makedirs(savedpath_model, exist_ok=True)
+    torch.save(best_model, os.path.join(savedpath_model, "model.pt"))
+
+    # load best model, and get test accuracy
+    model.load_state_dict(torch.load(os.path.join(savedpath_model, "model.pt"), map_location=device))
+    test_acc = graclus_test(model, test_loader, test_dataset)
+    print("best test accuracy is: ", test_acc)
+
+    # save best accuracy
+    savedpath_acc = f"pretrained_all/train_accuracy/{args.type}_{args.data}/"
+    if not os.path.isdir(savedpath_acc):
+        os.makedirs(savedpath_acc, exist_ok=True)
+    np.save(f"{savedpath_acc}/test_accuracy.npy", test_acc)
+
+
+if __name__ == "__main__":
+    args = Parser().parse()
+    train_and_save(args)
