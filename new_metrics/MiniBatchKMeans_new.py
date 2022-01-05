43231ee00e8e2a9c41bafaadb1db808ceeba19d0
yinghuali <yinghua.li@uni.lu>
2022-01-05 16:52:46 +0100

    update MiniBatchKMeans_new.py

--- new_metrics/MiniBatchKMeans_new.py
+++ new_metrics/MiniBatchKMeans_new.py
@@ -10,18 +10,16 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def MiniBatchKMeans_metrics(model, retrain_loader, select_num):
     x = get_predict_list(retrain_loader, model)
-    MBK = MiniBatchKMeans(n_components=10, random_state=0)
+    MBK = MiniBatchKMeans(n_clusters=select_num, random_state=0)
     MBK.fit(x)
     center_list = MBK.cluster_centers_
     select_index = []
-    n = int(select_num / 10)
     for p in center_list:
         distances_list = []
         for i in range(len(x)):
             distances = np.linalg.norm(x[i] - p)
             distances_list.append(distances)
-        index_center_list = list(np.argsort(distances_list)[:n])
-        select_index += index_center_list
-
+        index = np.argsort(distances_list)[:1][0]
+        select_index.append(index)
     return select_index
 
