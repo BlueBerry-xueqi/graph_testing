9aa6d3fddf0aebea4dd7527fdeee7ec2dc60c6c0
yinghuali <yinghua.li@uni.lu>
2023-09-14 10:35:48 +0200

    update

--- nodeImportance_retraining/start_retrain_original.sh
+++ nodeImportance_retraining/start_retrain_original.sh
@@ -13,4 +13,3 @@ python retraining_main.py --cuda 'cuda:1' --model_name 'gcn' --target_model_path
 python retraining_main.py --cuda 'cuda:1' --model_name 'gat' --target_model_path './target_models/pubmed_gat.pt' --path_x '../data/pubmed/x_np.pkl' --path_edge_index '../data/pubmed/edge_index_np.pkl' --path_y '../data/pubmed/y_np.pkl' --path_save './result/pubmed_gat.json'
 python retraining_main.py --cuda 'cuda:1' --model_name 'graphsage' --target_model_path './target_models/pubmed_graphsage.pt' --path_x '../data/pubmed/x_np.pkl' --path_edge_index '../data/pubmed/edge_index_np.pkl' --path_y '../data/pubmed/y_np.pkl' --path_save './result/pubmed_graphsage.json'
 python retraining_main.py --cuda 'cuda:1' --model_name 'tagcn' --target_model_path './target_models/pubmed_tagcn.pt' --path_x '../data/pubmed/x_np.pkl' --path_edge_index '../data/pubmed/edge_index_np.pkl' --path_y '../data/pubmed/y_np.pkl' --path_save './result/pubmed_tagcn.json'
-
